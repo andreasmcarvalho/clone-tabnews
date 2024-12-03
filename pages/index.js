533d5366e693a84c4a17f99bc09d752067df3479
andreasmcarvalho <andreasmcarvalho1@gmail.com>
2024-12-03 22:25:20 +0000

    alterando o nome do site

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Para Gabriele ❤️</title>
+        <title>Para Gabrielle ❤️</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <div className={styles.card}>
